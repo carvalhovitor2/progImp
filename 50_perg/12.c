5180c9e0a19d688a833135ad03208c7f23c1c1e8
Vitor Carvalho <vac@eurotux.com>
2020-03-01 13:54:28 +0000

    Works for consecutive vowels aswell

--- 50_perg/12.c
+++ 50_perg/12.c
@@ -3,7 +3,7 @@
 
 void *newStrnoV(char* s1){
 	for(int i=0;*(s1+i) != '\0'; i++){
-		if( *(s1+i) == 'a' || *(s1+i) == 'e' || *(s1+i) == 'i' || *(s1+i) == 'o' || *(s1+i) == 'u' || *(s1+i) == 'A' || *(s1+i) == 'E' || *(s1+i) == 'I' || *(s1+i) == 'O' || *(s1+i) == 'U' ){
+		while( *(s1+i) == 'a' || *(s1+i) == 'e' || *(s1+i) == 'i' || *(s1+i) == 'o' || *(s1+i) == 'u' || *(s1+i) == 'A' || *(s1+i) == 'E' || *(s1+i) == 'I' || *(s1+i) == 'O' || *(s1+i) == 'U' ){
 			for(int j = i;*(s1+j) != '\0';j++){
 				*(s1+j) = *(s1+j+1);
 			}
@@ -13,7 +13,7 @@ void *newStrnoV(char* s1){
 }
 
 int main(){
-	char s1[100], s2[100];
+	char s1[1000];
 	//Yes, I know gets is not secure
 	gets(s1);
 	newStrnoV(s1);
